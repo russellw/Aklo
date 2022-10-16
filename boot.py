848ae1766d1402317e8e30ac14f871bfc9f88290
Russell Wallace <russell.wallace@gmail.com>
2022-10-16 17:52:15 +0100

    tokenizer

--- boot.py
+++ boot.py
@@ -1,23 +1,153 @@
 import sys
 
-
+# parser
 def parse(filename):
     text = open(filename).read()
     ti = 0
     line = 1
+
+    dentc = None
+    cols = [0]
+    dedents = 0
+
     tok = None
 
+    def err(msg):
+        raise Exception(f"{filename}:{line}: {msg}")
+
+    # tokenizer
     def lex():
+        nonlocal dedents
+        nonlocal dentc
         nonlocal line
-        while 1:
-            c = tok[ti]
+        nonlocal ti
+        nonlocal tok
+
+        if dedents:
+            dedents -= 1
+            tok = ".dedent"
+            return
+
+        while ti < len(text):
+            i = ti
+            tok = text[ti]
+
+            # newline
+            if text[ti] == "\n":
+                # next line
+                ti += 1
+                if ti == len(text):
+                    break
+                line += 1
+
+                # measure indent
+                col = 0
+                while text[ti] == "\t" or text[ti] == " ":
+                    if text[ti] != dentc and dentc:
+                        err("indented with tabs and spaces in same file")
+                    dentc = text[ti]
+                    ti += 1
+                    col += 1
+
+                # nothing on this line
+                if text[ti] in ("\n", ";") or text[ti : ti + 2] == "/*":
+                    continue
+
+                # one indent
+                if col > cols[-1]:
+                    cols.append(col)
+                    tok = ".indent"
+                    return
+
+                # zero or more dedents
+                while col < cols[-1]:
+                    del cols[-1]
+                    dedents += 1
+                if col != cols[-1]:
+                    err("inconsistent indent")
+                return
 
             # space
-            if c.isspace():
+            if text[ti].isspace():
                 ti += 1
                 continue
 
+            # comment
+            if text[ti] == ";":
+                while text[ti] != "\n":
+                    ti += 1
+                continue
+            if text[ti : ti + 2] == "/*":
+                ti += 2
+                line1 = line
+                while text[ti : ti + 2] != "*/":
+                    if ti == len(text):
+                        line = line1
+                        err("unclosed block comment")
+                    if text[ti] == "\n":
+                        line += 1
+                    ti += 1
+                ti += 2
+                continue
+
+            # word or number
+            if text[ti].isalnum() or text[ti] == "_":
+                while text[ti].isalnum() or text[ti] in ("_", "?"):
+                    ti += 1
+                tok = text[i:ti]
+                return
+
+            # quote
+            if text[ti] in ("'", '"'):
+                q = text[ti]
+                ti += 1
+                while text[ti] != q:
+                    # TODO: handle or disallow multiline strings
+                    if text[ti] == "\\":
+                        ti += 1
+                    ti += 1
+                ti += 1
+                tok = text[i:ti]
+                return
+
+            # punctuation
+            punct = (
+                "!=",
+                "&&",
+                "++",
+                "+=",
+                "--",
+                "-=",
+                "<=",
+                "==",
+                "==",
+                ">=",
+                "||",
+            )
+            for s in punct:
+                if text[ti : ti + len(s)] == s:
+                    ti += len(s)
+                    tok = s
+                    return
+
+            ti += 1
+            return
+
+        # end of file
+        tok = ".dedent"
+
+    def eat(s):
+        if tok == s:
+            lex()
+            return 1
+
+    def expect(s):
+        if not eat(s):
+            err(f"'{repr(tok)}': expected '{repr(s)}'")
+
     lex()
+    eat("\n")
+    print(tok)
 
 
 parse(sys.argv[1])
