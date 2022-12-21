dfa5eaac61f148ef4b10f7f288e402d785d8f366
Russell Wallace <russell.wallace@gmail.com>
2022-12-21 11:32:32 +0000

    rem

--- src/main/java/aklo/Rem.java
+++ src/main/java/aklo/Rem.java
@@ -1,8 +1,24 @@
 package aklo;
 
+
 public final class Rem extends Term2 {
   public Rem(Loc loc, Term arg0, Term arg1) {
     super(loc, arg0, arg1);
+  }
+
+  @Override
+  public double apply(double a, double b) {
+    return a % b;
+  }
+
+  @Override
+  public float apply(float a, float b) {
+    return a % b;
+  }
+
+  @Override
+  public BigRational apply(BigRational a, BigRational b) {
+    return a.add(b);
   }
 
   @Override
