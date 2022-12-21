3a9b850d53b10016d73d1208d36e03ac93583665
Russell Wallace <russell.wallace@gmail.com>
2022-12-21 10:05:43 +0000

    eval unary

--- src/main/java/aklo/Neg.java
+++ src/main/java/aklo/Neg.java
@@ -1,8 +1,30 @@
 package aklo;
 
+import java.math.BigInteger;
+
 public final class Neg extends Term1 {
   public Neg(Loc loc, Term arg) {
     super(loc, arg);
+  }
+
+  @Override
+  public double apply(double a) {
+    return -a;
+  }
+
+  @Override
+  public float apply(float a) {
+    return -a;
+  }
+
+  @Override
+  public BigInteger apply(BigInteger a) {
+    return a.negate();
+  }
+
+  @Override
+  public BigRational apply(BigRational a) {
+    return a.negate();
   }
 
   @Override
--- src/main/java/aklo/Term1.java
+++ src/main/java/aklo/Term1.java
@@ -1,5 +1,6 @@
 package aklo;
 
+import java.math.BigInteger;
 import java.util.Iterator;
 
 public abstract class Term1 extends Term {
@@ -24,6 +25,30 @@ public abstract class Term1 extends Term {
   public Term get(int i) {
     assert i == 0;
     return arg;
+  }
+
+  public double apply(double a) {
+    throw new UnsupportedOperationException(toString());
+  }
+
+  public float apply(float a) {
+    throw new UnsupportedOperationException(toString());
+  }
+
+  public BigInteger apply(BigInteger a) {
+    throw new UnsupportedOperationException(toString());
+  }
+
+  public BigRational apply(BigRational a) {
+    throw new UnsupportedOperationException(toString());
+  }
+
+  @Override
+  public Term eval() {
+    if (arg instanceof ConstDouble) return new ConstDouble(loc, apply(arg.doubleVal()));
+    if (arg instanceof ConstFloat) return new ConstFloat(loc, apply(arg.floatVal()));
+    if (arg instanceof ConstRational) return new ConstRational(loc, apply(arg.rationalVal()));
+    return new ConstInteger(loc, apply(arg.integerVal()));
   }
 
   @Override
