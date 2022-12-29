baf8fe53da19ebdc1871f59c89f1a2260b8bc6db
Russell Wallace <russell.wallace@gmail.com>
2022-12-29 22:08:19 +0000

    refactor

--- src/main/java/aklo/Program.java
+++ src/main/java/aklo/Program.java
@@ -7,7 +7,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
 import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.Label;
 
 public final class Program {
   public final List<Var> vars = new ArrayList<>();
@@ -21,27 +20,20 @@ public final class Program {
   }
 
   public void write() throws IOException {
-    ClassWriter cw = new ClassWriter(0);
-    cw.visit(V17, ACC_PUBLIC, "a", null, "java/lang/Object", new String[0]);
-    cw.visitSource("a.java", null);
-
+    var w = new ClassWriter(0);
+    w.visit(V17, ACC_PUBLIC, "a", null, "java/lang/Object", new String[0]);
+    w.visitSource("a.java", null);
     for (var f : fns) {
-      Etc.dbg(f.blocks);
-      var mv =
-          cw.visitMethod(ACC_PUBLIC | ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
-      mv.visitCode();
-      Label label0 = new Label();
-      mv.visitLabel(label0);
-      mv.visitLineNumber(2, label0);
+      var v = w.visitMethod(ACC_PUBLIC | ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
+      v.visitCode();
 
-      mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "()V", false);
+      v.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "()V", false);
 
-      mv.visitInsn(RETURN);
-      mv.visitMaxs(1, 1);
-      mv.visitEnd();
+      v.visitInsn(RETURN);
+      v.visitMaxs(1, 1);
+      v.visitEnd();
     }
-
-    cw.visitEnd();
-    Files.write(Path.of("a.class"), cw.toByteArray());
+    w.visitEnd();
+    Files.write(Path.of("a.class"), w.toByteArray());
   }
 }
