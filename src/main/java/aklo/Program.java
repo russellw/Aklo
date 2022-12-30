6a09f85ed0e8def5307cbe72339a2fb6565b9380
Russell Wallace <russell.wallace@gmail.com>
2022-12-30 16:26:39 +0000

    represent nontrivial constants as static final fields

--- src/main/java/aklo/Program.java
+++ src/main/java/aklo/Program.java
@@ -3,6 +3,7 @@ package aklo;
 import static org.objectweb.asm.Opcodes.*;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
@@ -24,7 +25,7 @@ public final class Program {
     w.visit(V17, ACC_PUBLIC, "a", null, "java/lang/Object", new String[0]);
 
     // represent nontrivial constants as static final fields
-    var consts = new HashMap<Object, Var>();
+    var consts = new LinkedHashMap<Object, Var>();
     for (var f : fns)
       for (var block : f.blocks)
         for (var a : block.insns)
@@ -39,20 +40,61 @@ public final class Program {
                       x.type = b.type();
                       x.val = val;
                       consts.put(val, x);
-                      vars.add(x);
+
+                      var fv =
+                          w.visitField(
+                              ACC_PRIVATE | ACC_STATIC | ACC_FINAL,
+                              x.name,
+                              x.type.descriptor(),
+                              null,
+                              null);
+                      fv.visitEnd();
                     }
                   }
                 }
               });
+    var mv = w.visitMethod(ACC_STATIC, "<clinit>", "()V", null, null);
+    mv.visitCode();
+    for (var x : consts.values()) {
+      switch (x.type.kind()) {
+        case INTEGER -> {
+          var val = (BigInteger) x.val;
+          try {
+            mv.visitLdcInsn(val.longValueExact());
+            mv.visitMethodInsn(
+                INVOKESTATIC,
+                "java/math/BigInteger",
+                "valueOf",
+                "(J)Ljava/math/BigInteger;",
+                false);
+          } catch (ArithmeticException e) {
+            // okay to use an exception for something that is not an error here
+            // because constant integers outside 2^63 are rare enough
+            // that there is no performance impact
+            mv.visitTypeInsn(NEW, "java/math/BigInteger");
+            mv.visitInsn(DUP);
+            mv.visitLdcInsn(val.toString(16));
+            mv.visitIntInsn(BIPUSH, 16);
+            mv.visitMethodInsn(
+                INVOKESPECIAL, "java/math/BigInteger", "<init>", "(Ljava/lang/String;I)V", false);
+          }
+          mv.visitFieldInsn(PUTSTATIC, "a", x.name, "Ljava/math/BigInteger;");
+        }
+        default -> throw new IllegalStateException(x.toString());
+      }
+      mv.visitInsn(RETURN);
+      mv.visitMaxs(0, 0);
+      mv.visitEnd();
+    }
 
     // functions
     for (var f : fns) {
-      var v = w.visitMethod(ACC_PUBLIC | ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
-      v.visitCode();
+      mv = w.visitMethod(ACC_PUBLIC | ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
+      mv.visitCode();
 
-      v.visitInsn(RETURN);
-      v.visitMaxs(0, 0);
-      v.visitEnd();
+      mv.visitInsn(RETURN);
+      mv.visitMaxs(0, 0);
+      mv.visitEnd();
     }
     w.visitEnd();
 
--- src/main/java/aklo/Type.java
+++ src/main/java/aklo/Type.java
@@ -3,8 +3,15 @@ package aklo;
 public abstract class Type {
   public abstract Kind kind();
 
+  public abstract String descriptor();
+
   public static final Type BOOL =
       new Type() {
+        @Override
+        public String descriptor() {
+          return "Z";
+        }
+
         @Override
         public Kind kind() {
           return Kind.BOOL;
@@ -16,9 +23,19 @@ public abstract class Type {
         public Kind kind() {
           return Kind.VOID;
         }
+
+        @Override
+        public String descriptor() {
+          return "V";
+        }
       };
   public static final Type FLOAT =
       new Type() {
+        @Override
+        public String descriptor() {
+          return "F";
+        }
+
         @Override
         public Kind kind() {
           return Kind.FLOAT;
@@ -26,6 +43,11 @@ public abstract class Type {
       };
   public static final Type DOUBLE =
       new Type() {
+        @Override
+        public String descriptor() {
+          return "D";
+        }
+
         @Override
         public Kind kind() {
           return Kind.DOUBLE;
@@ -37,12 +59,22 @@ public abstract class Type {
         public Kind kind() {
           return Kind.INTEGER;
         }
+
+        @Override
+        public String descriptor() {
+          return "Ljava/math/BigInteger;";
+        }
       };
   public static final Type RATIONAL =
       new Type() {
         @Override
         public Kind kind() {
           return Kind.RATIONAL;
+        }
+
+        @Override
+        public String descriptor() {
+          return "Laklo/BigRational;";
         }
       };
 }
