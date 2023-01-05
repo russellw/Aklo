a9edf6d4b35cbf426582bf204bda81ae25eeb7ef
Russell Wallace <russell.wallace@gmail.com>
2023-01-05 19:26:14 +0000

    rem

--- src/main/java/aklo/Rem.java
+++ src/main/java/aklo/Rem.java
@@ -1,23 +1,34 @@
 package aklo;
 
+import static org.objectweb.asm.Opcodes.INVOKESTATIC;
+
+import java.math.BigInteger;
+import org.objectweb.asm.MethodVisitor;
+
 public final class Rem extends Term2 {
   public Rem(Loc loc, Term arg0, Term arg1) {
     super(loc, arg0, arg1);
   }
 
   @Override
-  public double apply(double a, double b) {
-    return a % b;
+  public Object apply(BigInteger a, BigInteger b) {
+    return a.remainder(b);
   }
 
   @Override
-  public float apply(float a, float b) {
-    return a % b;
+  public void emit(MethodVisitor mv) {
+    arg0.load(mv);
+    arg1.load(mv);
+    mv.visitMethodInsn(
+        INVOKESTATIC,
+        "aklo/Rem",
+        "eval",
+        "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;",
+        false);
   }
 
-  @Override
-  public BigRational apply(BigRational a, BigRational b) {
-    return a.add(b);
+  public static Object eval(Object a, Object b) {
+    return Term2.eval(new Rem(null, null, null), a, b);
   }
 
   @Override
