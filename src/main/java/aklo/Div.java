47fab8d6e6f126ae8b7569ffc37572eb337f6685
Russell Wallace <russell.wallace@gmail.com>
2023-01-05 15:51:29 +0000

    fdiv

--- src/main/java/aklo/Div.java
+++ src/main/java/aklo/Div.java
@@ -1,8 +1,49 @@
 package aklo;
 
+import static org.objectweb.asm.Opcodes.INVOKESTATIC;
+
+import java.math.BigInteger;
+import org.objectweb.asm.MethodVisitor;
+
 public final class Div extends Term2 {
   public Div(Loc loc, Term arg0, Term arg1) {
     super(loc, arg0, arg1);
+  }
+
+  @Override
+  public void emit(MethodVisitor mv) {
+    arg0.load(mv);
+    arg1.load(mv);
+    mv.visitMethodInsn(
+        INVOKESTATIC,
+        "aklo/Div",
+        "run",
+        "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;",
+        false);
+  }
+
+  public static Object run(Object a, Object b) {
+    return Term2.run(new Div(null, null, null), a, b);
+  }
+
+  @Override
+  public double apply(double a, double b) {
+    return a / b;
+  }
+
+  @Override
+  public float apply(float a, float b) {
+    return a / b;
+  }
+
+  @Override
+  public BigInteger apply(BigInteger a, BigInteger b) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public BigRational apply(BigRational a, BigRational b) {
+    return a.divide(b);
   }
 
   @Override
