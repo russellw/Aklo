2c298b6b3c7edf8aef18342dd58f74e64c05c5c6
Russell Wallace <russell.wallace@gmail.com>
2023-01-06 03:12:29 +0000

    comparison

--- src/main/java/aklo/EqNumber.java
+++ src/main/java/aklo/EqNumber.java
@@ -1,5 +1,10 @@
 package aklo;
 
+import static org.objectweb.asm.Opcodes.INVOKESTATIC;
+
+import java.math.BigInteger;
+import org.objectweb.asm.MethodVisitor;
+
 public final class EqNumber extends Term2 {
   public EqNumber(Loc loc, Term arg0, Term arg1) {
     super(loc, arg0, arg1);
@@ -8,5 +13,41 @@ public final class EqNumber extends Term2 {
   @Override
   public Tag tag() {
     return Tag.EQ_NUMBER;
+  }
+
+  @Override
+  public void emit(MethodVisitor mv) {
+    arg0.load(mv);
+    arg1.load(mv);
+    mv.visitMethodInsn(
+        INVOKESTATIC,
+        "aklo/EqNumber",
+        "eval",
+        "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;",
+        false);
+  }
+
+  public static Object eval(Object a, Object b) {
+    return Term2.eval(new EqNumber(null, null, null), a, b);
+  }
+
+  @Override
+  public Object apply(double a, double b) {
+    return a == b;
+  }
+
+  @Override
+  public Object apply(float a, float b) {
+    return a == b;
+  }
+
+  @Override
+  public Object apply(BigInteger a, BigInteger b) {
+    return a.equals(b);
+  }
+
+  @Override
+  public Object apply(BigRational a, BigRational b) {
+    return a.equals(b);
   }
 }
--- src/main/java/aklo/Le.java
+++ src/main/java/aklo/Le.java
@@ -1,5 +1,10 @@
 package aklo;
 
+import static org.objectweb.asm.Opcodes.INVOKESTATIC;
+
+import java.math.BigInteger;
+import org.objectweb.asm.MethodVisitor;
+
 public final class Le extends Term2 {
   public Le(Loc loc, Term arg0, Term arg1) {
     super(loc, arg0, arg1);
@@ -8,5 +13,41 @@ public final class Le extends Term2 {
   @Override
   public Tag tag() {
     return Tag.LE;
+  }
+
+  @Override
+  public void emit(MethodVisitor mv) {
+    arg0.load(mv);
+    arg1.load(mv);
+    mv.visitMethodInsn(
+        INVOKESTATIC,
+        "aklo/Le",
+        "eval",
+        "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;",
+        false);
+  }
+
+  public static Object eval(Object a, Object b) {
+    return Term2.eval(new Le(null, null, null), a, b);
+  }
+
+  @Override
+  public Object apply(double a, double b) {
+    return a <= b;
+  }
+
+  @Override
+  public Object apply(float a, float b) {
+    return a <= b;
+  }
+
+  @Override
+  public Object apply(BigInteger a, BigInteger b) {
+    return a.compareTo(b) <= 0;
+  }
+
+  @Override
+  public Object apply(BigRational a, BigRational b) {
+    return a.compareTo(b) <= 0;
   }
 }
