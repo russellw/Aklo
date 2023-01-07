4e807642ccb617265370e0703f2296d5d6656b71
Russell Wallace <russell.wallace@gmail.com>
2023-01-07 00:26:20 +0000

    exp

--- src/main/java/aklo/Exp.java
+++ src/main/java/aklo/Exp.java
@@ -1,8 +1,50 @@
 package aklo;
 
+import static org.objectweb.asm.Opcodes.INVOKESTATIC;
+
+import java.math.BigInteger;
+import org.objectweb.asm.MethodVisitor;
+
 public final class Exp extends Term2 {
   public Exp(Loc loc, Term arg0, Term arg1) {
     super(loc, arg0, arg1);
+  }
+
+  @Override
+  public void emit(MethodVisitor mv) {
+    arg0.load(mv);
+    arg1.load(mv);
+    mv.visitMethodInsn(
+        INVOKESTATIC,
+        "aklo/Exp",
+        "eval",
+        "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;",
+        false);
+  }
+
+  public static Object eval(Object a, Object b) {
+    return Term2.eval(new Exp(null, null, null), a, b);
+  }
+
+  @Override
+  public Object apply(double a, double b) {
+    return Math.pow(a, b);
+  }
+
+  @Override
+  public Object apply(float a, float b) {
+    return Math.pow(a, b);
+  }
+
+  @Override
+  public Object apply(BigInteger a, BigInteger b) {
+    return a.pow(b.intValueExact());
+  }
+
+  @Override
+  public Object apply(BigRational a, BigRational b) {
+    // TODO
+    throw new UnsupportedOperationException();
   }
 
   @Override
